31eb73ee2f94fdd95c8a23e0d5132740419b8552
Isaias Cordova <igerardo0808@gmail.com>
2024-05-21 14:03:09 -0600

    dibujo de triangulito

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,8 @@ class DibujarApp extends StatefulWidget {
 }
 
 class _DibujarAppState extends State<DibujarApp> {
-  int indice = 0;
-  List<CustomPainter> _listaDibujos = [CuadradoPainter()];
+  int indice = 1;
+  List<CustomPainter> _listaDibujos = [CuadradoPainter(), TrianguloPainter()];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -33,8 +33,11 @@ class _DibujarAppState extends State<DibujarApp> {
               ),
             ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 dibujarCuadrado(),
+                dibujarTriangulo(),
               ],
             )
           ],
@@ -48,6 +51,16 @@ class _DibujarAppState extends State<DibujarApp> {
         onPressed: () => {
               setState(() {
                 indice = 0;
+              })
+            },
+        child: Text('Cuadrado'));
+  }
+
+  Widget dibujarTriangulo() {
+    return ElevatedButton(
+        onPressed: () => {
+              setState(() {
+                indice = 1;
               })
             },
         child: Text('Cuadrado'));
@@ -70,6 +83,34 @@ class CuadradoPainter extends CustomPainter {
         Offset(size.width - 20, size.height - 50), paint); // _
     canvas.drawLine(Offset(size.width - 20, 50),
         Offset(size.width - 20, size.height - 50), paint); // |
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return false;
+  }
+}
+
+class TrianguloPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO: implement paint
+    Paint paint = Paint();
+    paint.strokeWidth = 2;
+    paint.color = Colors.black;
+
+    // Inicio(x,y) | Fin(x,y)
+    canvas.drawLine(
+        Offset(size.width / 2, 20), Offset(20, size.height - 100), paint);
+
+    canvas.drawLine(
+        Offset(20, size.height - 100), //  /
+        Offset(size.width - 20, size.height - 100),
+        paint); // _
+
+    canvas.drawLine(Offset(size.width - 20, size.height - 100),
+        Offset(size.width / 2, 20), paint);
   }
 
   @override
